3f7338296c6758e9bdee73168d884d5854e302ce
tzeitim <pedroy.final@gmail.com>
2025-10-15 19:31:12 +0300

    added corresponding new objects

--- smolscells/__init__.py
+++ smolscells/__init__.py
@@ -1,9 +1,17 @@
 import logging
 
-# Configure logging for the package
-
 from . import sim
+from .lineage_treedata import LineageForest
+from .sim import SimulatedLineageForest, simulate_lineage_experiment
+from . import tree_node_matching as tnm
 
+__all__ = [
+    "LineageForest",
+    "SimulatedLineageForest",
+    "simulate_lineage_experiment",
+    "sim",
+    "tnm",
+]
 
 logging.basicConfig(
     level=logging.INFO,
